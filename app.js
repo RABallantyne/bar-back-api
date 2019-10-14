1734e3a55d8e2f77135a85dcb90e3073c7f2d8fa
Jerkbert <raballantyne@gmail.com>
2019-10-14 17:16:25 -0600

    security

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
 const bars = require("./controllers/bars");
 require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
+app.use(helmet());
+
 app.use("/bars", bars);
 
 const port = process.env.PORT || 3000;
